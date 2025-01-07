5308c9f0c53bf533ebb46112aac7e42490f31fe9
sukunacutie <mjnavarro80@gmail.com>
2025-01-07 22:59:15 +0800

    01-07-25

--- LEETCODE_75/ARRAY/8.py
+++ LEETCODE_75/ARRAY/8.py
@@ -6,13 +6,11 @@ class Solution:
         n = len(nums)
         answer = [1] * n
 
-    # Calculate prefix products
         prefix = 1
         for i in range(n):
             answer[i] = prefix
             prefix *= nums[i]
-
-        # Calculate suffix products and combine with prefix
+            
         suffix = 1
         for i in range(n - 1, -1, -1):
             answer[i] *= suffix
